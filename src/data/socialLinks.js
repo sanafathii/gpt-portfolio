b60ac8477078c54ed5db8e78d02fc7e93a0ad305
sana <sana.fathi30@gmail.com>
2025-02-03 14:54:21 +0330

    fixed socialLinks.js

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -11,28 +11,24 @@ const socialLinks = [{
         platform: "LinkedIn",
         link: "https://www.linkedin.com/in/sanafathi/",
         icon: < FontAwesomeIcon icon = { faLinkedin }
-        size = "2x" / > ,
+        />,
+        size: "2x",
     },
     {
         id: 2,
         platform: "GitHub",
         link: "https://github.com/sanafathii",
         icon: < FontAwesomeIcon icon = { faGithub }
-        size = "2x" / > ,
+        />,
+        size: "2x",
     },
     {
         id: 3,
-        platform: "Leetcode",
-        link: "https://leetcode.com/sanafathi/",
-        icon: < FontAwesomeIcon icon = { faLeetcode }
-        size = "2x" / > ,
-    },
-    {
-        id: 4,
         platform: "Email",
         link: "mailto:sana.fathi30@gmail.com",
         icon: < FontAwesomeIcon icon = { faEnvelope }
-        size = "2x" / > ,
+        />,
+        size: "2x",
     },
 ];
 
